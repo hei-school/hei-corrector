970d001e5561468fccc3cb4b1b101ab8ad87532d
Lou Andria <lou@hei.school>
2022-12-04 00:14:43 +0100

    feat: correct Q21P2

--- src/main/java/school/hei/corrector/session021222/StdAnswers021222.java
+++ src/main/java/school/hei/corrector/session021222/StdAnswers021222.java
@@ -1,27 +1,19 @@
 package school.hei.corrector.session021222;
 
-import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
 import school.hei.corrector.StdAnswers;
 import school.hei.utils.Log;
 
 import java.io.File;
-
-import static org.eclipse.jgit.api.Git.cloneRepository;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public record StdAnswers021222(
         String stdRef,
         String q21p1, String q21p2, String q21p3, String q21p4, String q21p5, String q21p6)
         implements StdAnswers {
-
-    private static final int RANDOM_APPENDIX_TO_REPO_FOLDER_NAME =
-            // so that scoring is idempotent
-            (int) (Math.random() * 1_000_000);
-
-    private String getRepoName() {
-        return ExamSession021222.class.getSimpleName() + " - " + stdRef + " - " + RANDOM_APPENDIX_TO_REPO_FOLDER_NAME;
-    }
 
     @Override
     public int correct() {
@@ -29,6 +21,7 @@ public record StdAnswers021222(
         int score = 0;
 
         score = score + correctQ21P1();
+        score = score + correctQ21P2();
 
         Log.info("[... Correction d'un étudiant] Réf étudiante : " + stdRef + ", points obtenus : " + score);
         return score;
@@ -36,21 +29,78 @@ public record StdAnswers021222(
 
     public int correctQ21P1() {
         try {
-            cloneRepo();
+            Log.info("[Q21P1...] Clonage de la dev depuis " + q21p1);
+            cloneRepo(randomRepoName());
+            Log.info("[...Q21P1] 1 point.");
             return 1;
         } catch (Exception e) {
-            Log.info("[... Correction d'un étudiant] Réf étudiante : " + stdRef + ", erreur : " + e);
+            Log.error("[...Q21P1] " + e);
             return 0;
         }
     }
 
-    private Git cloneRepo() throws GitAPIException {
-        CloneCommand cloneCommand = cloneRepository()
+    public int correctQ21P2() {
+        try {
+            Log.info("[Q21P2...] Vérification du commit : " + q21p2);
+
+            String randomRepoName = randomRepoName();
+            Git git = cloneRepo(randomRepoName);
+
+            if (!branchHasCommit(git, "dev", q21p2)) {
+                Log.error("[...Q21P2] La dev ne contient pas le commit : " + q21p2);
+                return 0;
+            }
+
+            git
+                    .checkout()
+                    .setName(q21p2)
+                    .call();
+            var vazo = Files.readAllLines(Path.of(randomRepoName + "/vazo.txt"));
+            var vazoSize = vazo.size();
+            if (vazoSize != 1) {
+                Log.error("[...Q21P2] Vazo ne doit avoir que _1_ ligne au lieu de : " + vazoSize);
+                return 0;
+            }
+
+            var title = vazo.get(0).trim();
+            if (!title.contains("Vazon'ny lavitra")) {
+                Log.error("[...Q21P2] Le titre est mauvais : " + title);
+                return 0;
+            }
+
+            Log.info("[...Q21P2] 1 point.");
+            return 1;
+        } catch (Exception e) {
+            Log.error("[...Q21P2] " + e);
+            return 0;
+        }
+    }
+
+    private String randomRepoName() {
+        int randomAppendix = (int) (Math.random() * 1_000_000_000); // so that scoring is idempotent
+        return ExamSession021222.class.getSimpleName() + " - " + stdRef + " - " + randomAppendix;
+    }
+
+    private Git cloneRepo(String randomRepoName) throws GitAPIException {
+        var git = Git
+                .cloneRepository()
                 .setCloneSubmodules(true)
                 .setURI(q21p1)
                 .setBranch("dev")
                 .setCloneSubmodules(true)
-                .setDirectory(new File(getRepoName()));
-        return cloneCommand.call();
+                .setDirectory(new File(randomRepoName))
+                .call();
+        Log.info("... clonage réussi dans : " + randomRepoName);
+        return git;
+    }
+
+    private boolean branchHasCommit(Git git, String branchName, String commitSha) throws GitAPIException {
+        return git.
+                branchList()
+                .setContains(commitSha)
+                .call()
+                .stream()
+                .map(Ref::getName)
+                .anyMatch(refName -> refName.contains(branchName));
     }
 }
--- src/test/java/school/hei/corrector/session021222/StdAnswers021222Test.java
+++ src/test/java/school/hei/corrector/session021222/StdAnswers021222Test.java
@@ -15,4 +15,13 @@ class StdAnswers021222Test {
 
         assertEquals(1, score);
     }
+
+    @Test
+    void on_examSession021222_std21065_has_correct_q21p2() {
+        StdAnswers021222 answers = examSession021222_std21065();
+
+        var score = answers.correctQ21P2();
+
+        assertEquals(1, score);
+    }
 }
